f382e196af6e979b0177a6668204bed207a8449e
Izan <ios2asir@gmail.com>
2018-03-06 09:27:26 +0100

    Conversion binaria y hexadecimal

--- Excepciones/src/com/iesvirgendelcarmen/ejercicios/HexConversion.java
+++ Excepciones/src/com/iesvirgendelcarmen/ejercicios/HexConversion.java
@@ -19,18 +19,57 @@ public class HexConversion {
 		this.value = value;
 	}
 
-	public boolean checkHexadecimal(String value) {
-		return value.matches("[0123456789ABCDEF]+");
+	private static boolean checkHexadecimal(String value) {
+		return value.toUpperCase().matches("[0123456789ABCDEF]+");
 	}
 
-	public int getStringToHex(){
+	public int getDecimalFromHex(){
 		int decimal = Integer.parseInt(value, 16);
 		return decimal;
 	}
+	
+	public int getDecimal() {
+		StringBuilder sbinary = new StringBuilder(value);
+		String sbReverse = sbinary.reverse().toString();
+		int decimal = 0;
+		
+		for (int i = 0; i < sbReverse.length(); i++) {
+			char digit = sbReverse.charAt(i);
+			switch (digit) {
+			case 'a':
+				decimal+=Math.pow(16, i)*10;
+				break;
+			case 'b':
+				decimal+=Math.pow(16, i)*11;
+				break;
+			case 'c':
+				decimal+=Math.pow(16, i)*12;
+				break;
+			case 'd':
+				decimal+=Math.pow(16, i)*13;
+				break;
+			case 'e':
+				decimal+=Math.pow(16, i)*14;
+				break;
+			case 'f':
+				decimal+=Math.pow(16, i)*15;
+				break;
+				
+			default:
+				decimal+=Math.pow(16, i)*(digit-48);
+				break;
+			}
+		}
+		
+		return decimal;
+		
+		
+	}
+	
 
 	@Override
 	public String toString() {
-		return "HexConversion [getValue()=" + getValue() + ", getStringToHex()=" + getStringToHex() + "]";
+		return "HexConversion [getValue()=" + getValue() + ", getStringToHex()=" + getDecimalFromHex() + "]";
 	}
 	
 	
--- Excepciones/src/com/iesvirgendelcarmen/ejercicios/TestBinHex.java
+++ Excepciones/src/com/iesvirgendelcarmen/ejercicios/TestBinHex.java
@@ -1,11 +1,18 @@
 package com.iesvirgendelcarmen.ejercicios;
 
+import java.util.Scanner;
+
 public class TestBinHex {
 	
 	public static void main(String[] args) { 
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduce Binary number");
+		String inBin = sc.next();
+	
 		try {
-			BinConversion bin1 = new BinConversion("110");
+			BinConversion bin1 = new BinConversion(inBin);
 			System.out.printf("Decimal value of %s is %d%n",bin1.getValue(),
 					bin1.getBinaryToDecimal());
 
@@ -13,12 +20,17 @@ public class TestBinHex {
 			System.out.println(e);
 		}
 		
+		System.out.println("Introduce Hex number");
+		String inHex = sc.next();
+		
 		try {
-			HexConversion hex1 = new HexConversion("10293AEF");
-			System.out.printf("Decimal value of %s is %d%n",hex1.getValue(),
-					hex1.getStringToHex());
+			HexConversion hex1 = new HexConversion(inHex);
+			System.out.printf("Decimal value of %s is %d%n"
+					+ "With another method: %d%n",hex1.getValue(),
+					hex1.getDecimalFromHex(),hex1.getDecimal());
 		} catch (HexadecimalException e) {
 			System.out.println(e);
 		}
+
 	}
 }
