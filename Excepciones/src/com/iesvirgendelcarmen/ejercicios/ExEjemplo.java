4977493a5f88cda8d8cc453ef3a8ff9e8c3278e1
Izan <ios2asir@gmail.com>
2018-05-02 10:59:15 +0200

    Añadido contenido olvidado

---
+++ Excepciones/src/com/iesvirgendelcarmen/ejercicios/ExEjemplo.java
@@ -0,0 +1,10 @@
+package com.iesvirgendelcarmen.ejercicios;
+
+public class ExEjemplo extends Exception {
+
+	public ExEjemplo(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
